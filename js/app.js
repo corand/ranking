b51641775cdac65e06f055ae8a324b72b488366e
Andoni <andoni.correas@gmail.com>
2015-12-30 14:21:07 +0100

    interfazearen eguneraketa

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 var emaitzak = angular.module('emaitzak',[]);
 
 emaitzak.controller('MainCtrl', function($scope, $http) {
-  $scope.greeting = 'Hola!';
   $scope.aukeratua = "";
-  $http.get('js/emaitzak.json')
+  $http.get('js/ranking_masculino.json')
 	.then(function(res){
-	  $scope.frogak = res.data;                
+	  $scope.ranking_masculino = res.data;                
 	});
 
-    $scope.aukeratuFroga = function(emaitza){
-    	$scope.emaitzak = emaitza;
-    	$scope.aukeratua = emaitza.prueba;
+    $scope.aukeratuFroga = function(){
+    	console.log($scope.froga);
+    	$scope.emaitzak = $scope.froga;
     };
 });
