d02de42e34dea2c4162f58de41c01dd5440d71c0
corand <andoni.correas@gmail.com>
2016-01-07 09:43:23 +0100

    ng-disabled segundo select

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ranking
 Aloña Mendi Atletismo saileko denboren rankinga
+
+# Ikusgai
+[http://ranking.aloinargixao.com](http://ranking.aloinargixao.com)
--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ emaitzak.controller('MainCtrl', function($scope, $http) {
   var ranking_masculino,
   	  ranking_femenino;
   
+  $scope.genero_selected = false;
   $http.get('js/ranking_masculino.json')
 	.then(function(res){
 	  ranking_masculino = res.data;            
@@ -20,5 +21,6 @@ emaitzak.controller('MainCtrl', function($scope, $http) {
 
     $scope.generoChange = function(){
     	$scope.ranking = ($scope.genero === "Masculino") ? ranking_masculino : ($scope.genero === "Femenino") ? ranking_femenino : null;
+      $scope.genero_selected = true;
     };
 });
